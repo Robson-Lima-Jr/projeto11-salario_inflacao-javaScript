1998420dcf32d157687182a0615e1efde286d45d
Robson-Jr <robsonjuniorsn@gmail.com>
2024-02-09 18:17:15 -0300

    Projeto iniciado

---
+++ main.js
@@ -0,0 +1,13 @@
+let valores_gerais = [
+    {salario: 510, ipca: 5.91, ano: 2010},
+    {salario: 545, ipca: 6.50, ano: 2011},
+    {salario: 622, ipca: 5.84, ano: 2012},
+    {salario: 678, ipca: 5.91, ano: 2013},
+    {salario: 724, ipca: 6.41, ano: 2014},
+    {salario: 788, ipca: 10.67, ano: 2015},
+    {salario: 880, ipca: 6.29, ano: 2016},
+    {salario: 937, ipca: 2.95, ano: 2016},
+    {salario: 954, ipca: 3.75, ano: 2018},
+    {salario: 998, ipca: 4.31, ano: 2019},
+    {salario: 1045, ipca: 4.52, ano: 2020}
+];
